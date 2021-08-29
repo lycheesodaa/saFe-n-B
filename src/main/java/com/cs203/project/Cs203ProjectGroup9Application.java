2c4b4eebd9e2ffaf004c6b0aad4d8d99929f0dbf
Wei Soon <wscheong.2020@scis.smu.edu.sg>
2021-08-29 13:24:56 +0800

    Uploading Project Files

---
+++ src/main/java/com/cs203/project/Cs203ProjectGroup9Application.java
@@ -0,0 +1,13 @@
+package com.cs203.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs203ProjectGroup9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs203ProjectGroup9Application.class, args);
+	}
+
+}
