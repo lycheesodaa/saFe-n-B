83751cb09d68c914930324ac078924da4a907524
Wei Soon <wscheong.2020@scis.smu.edu.sg>
2021-11-09 14:11:09 +0800

    bug fixes

--- backend/src/main/java/com/cs203/project/users/employee/EmployeeRepository.java
+++ backend/src/main/java/com/cs203/project/users/employee/EmployeeRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.cs203.project.users.employee;
 
 import java.util.List;
@@ -11,14 +10,3 @@ public interface EmployeeRepository extends JpaRepository<Employee, String> {
 	public Employee findByEmail(String email);
 	public List<Employee> findByFirmEmail(String email);
 }
-=======
-package com.cs203.project.users.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-	public Employee findByEmail(String email);
-}
->>>>>>> 5edcc10ffe0cb908ab82af6dae873676e6872857
