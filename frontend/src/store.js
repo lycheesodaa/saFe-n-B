e86254872e3a07efa797e6f4d13feee8d53bde11
harshitjain2001 <89506205+harshitjain2001@users.noreply.github.com>
2021-11-10 12:15:26 +0800

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,8 +6,7 @@ import rootReducer from './reducers';
 const middleware = [thunk];
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
 ))
 
 export default store;
